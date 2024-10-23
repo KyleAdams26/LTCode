bb8a71fe7d482479410a07be82f327f2184073b6
KyleAdams26 <adams.k@ufl.edu>
2024-10-23 19:40:19 -0400

    Update getInitialConditions.m
    updated L0

--- getInitialConditions.m
+++ getInitialConditions.m
@@ -4,5 +4,5 @@ function IC = getInitialConditions() %gets initial conditions of state variables
     IC.Th0 = 70;
     IC.Tc0 = 100;
     IC.I0 = 0.0113;
-    IC.L0 = 2*(10^11);
+    IC.L0 = 2*(10^9);
 end
