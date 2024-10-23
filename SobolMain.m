87349702a06368b2df0f7186beef61fdc9922fa7
adams.k <adams.k@wvt-hp-ur2-047>
2024-10-22 20:49:41 -0400

    updated parameter ranges

--- SobolMain.m
+++ SobolMain.m
@@ -39,39 +39,39 @@ parsObj.dist = {'Uniform', 'Uniform' , 'Uniform',...
 nParameters = length(parsObj.name);
 
 parsObj.parameters = {
-    {'lower', 0.5,      'upper', 1.5},... %1lL
-    {'lower', 0.0416665,   'upper', 0.1249995},... %2dA;
-    {'lower', 0.025,     'upper', 0.075},... %3sR
-    {'lower', 0.03290,       'upper', 0.0987},... %4dR
-    {'lower', 0.304,       'upper', 0.912},...    %5aIR
-    {'lower', 0.004166665,      'upper', 0.012499995},... %6bIR
+    {'lower', 0.0000000005,      'upper', 0.000000005},... %1lL
+    {'lower', 0.04165,   'upper', 0.12495},... %2dA;
+    {'lower', 0.0535,     'upper', 0.1605},... %3sR
+    {'lower', 0.0329,       'upper', 0.0987},... %4dR
+    {'lower', 0.3125,       'upper', 0.9375},...    %5aIR
+    {'lower', 0.004165,      'upper', 0.012495},... %6bIR
     {'lower', 0.18,     'upper', 0.54},... %7aCI
-    {'lower', 175,   'upper', 525},... %8bCI
+    {'lower', 176,   'upper', 528},... %8bCI
     {'lower', 35.35,       'upper', 106.5},... %9aHI
-    {'lower', 50,       'upper', 150},... %10bHI
-    {'lower', 0.0025,   'upper', 0.0075},... %11lC
-    {'lower', 1.0395,     'upper', 3.1185},... %12gC
-    {'lower', 299.1,       'upper', 897.3},... %13KC
+    {'lower', 49.85,       'upper', 149.55},... %10bHI
+    {'lower', 0.0005,   'upper', 0.0015},... %11lC
+    {'lower', 1.04,     'upper', 3.12},... %12gC
+    {'lower', 299,       'upper', 897},... %13KC
     {'lower', 1,       'upper', 3},... %14aIC
     {'lower', 0.089,     'upper', 0.267},... %15bIC
-    {'lower', 0.000002999994,   'upper', 0.000008999982},... %16lH
-    {'lower', 0.756,     'upper', 2.268},... %17gH
-    {'lower', 211.2,       'upper', 633.6},... %18KH
+    {'lower', 0.00000315,   'upper', 0.00000945},... %16lH
+    {'lower', 0.755,     'upper', 2.265},... %17gH
+    {'lower', 211,       'upper', 633},... %18KH
     {'lower', 1,       'upper', 3},... %19aIH
     {'lower', 0.089,       'upper', .267},...    %20bIH
-    {'lower', 0.000001666666,      'upper', 0.000004999998},... %21IR
-    {'lower', 83.1775,        'upper', 249.5325},... %22dI
+    {'lower', 0.0000003335,      'upper', 0.0000010005},... %21lR
+    {'lower', 83,        'upper', 249},... %22dI
     {'lower', 0.5,     'upper', 1.5},... %23aHC
-    {'lower', 176,       'upper', 528},...    %24bHC
-    {'lower', 0.2926829269,       'upper', 0.8780487806},...%25dC
+    {'lower', 17.5,       'upper', 52.5},...    %24bHC
+    {'lower', 0.2925,       'upper', 0.8775},...%25dC
     {'lower', 0.00001305,     'upper', 0.00003915},...%26aAH
     {'lower', 2,     'upper', 6},... %27bAH   
     {'lower', 0.2,     'upper', 0.6},...%28aRA
     {'lower', 10,     'upper', 30},... %29bRA
     {'lower', 1,       'upper', 3},... %30aIRA   
-    {'lower', 178,      'upper', 534},... %31bIRA
+    {'lower', .178,      'upper', .534},... %31bIRA
     {'lower', 0.16665,   'upper', 0.49995},... %32dH
-    {'lower', 0.025,      'upper', 0.075},... %33dL
+    {'lower', 0.0025,      'upper', 0.0075},... %33dL
     {'lower', 5,     'upper', 15},... %34aCL
     {'lower', 100,       'upper', 300}};  %35bCL
 
