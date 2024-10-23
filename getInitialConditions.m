1741c67c8e303340f0a13088591b3ece39c0985b
adams.k <adams.k@wvt-hp-ur2-047>
2024-10-22 20:52:09 -0400

    updated initial values

--- getInitialConditions.m
+++ getInitialConditions.m
@@ -1,8 +1,8 @@
 function IC = getInitialConditions() %gets initial conditions of state variables
     IC.A0 = 8;
-    IC.Tr0 = 13.07;
+    IC.Tr0 = 1.31;
     IC.Th0 = 70;
     IC.Tc0 = 100;
-    IC.I0 = 0.00565;
-    IC.L0 = 2*(10^9);
+    IC.I0 = 0.0113;
+    IC.L0 = 2*(10^11);
 end
