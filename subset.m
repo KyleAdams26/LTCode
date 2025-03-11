a6fd1468f8837df95f09f5d8167efc27e0fb3d9b
KyleAdams26 <adams.k@ufl.edu>
2025-03-11 18:20:02 -0400

    Update subset.m
    adds explanation of what this function is and why I needed it

--- subset.m
+++ subset.m
@@ -1,3 +1,9 @@
+% this function was made to take a random subset of any bigger set. it was helpful in plotting QOI distributions
+% as the Sobol main file ends with a sample of (2N+1)*base samples QOI values, where N is # of parameters
+% so it was necessary to restrict the QOI distribution to a random subset in order to compare variability
+% produced by a different sets of parameters 
+
+
 % subset.m (with validation check)
 function random_subset = subset(data, subset_size)
     rng(59); % Set random seed
