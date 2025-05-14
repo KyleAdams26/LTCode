2c1ab933c5678a37b6876bc8948f9c9c8bfb2f16
KyleAdams26 <adams.k@ufl.edu>
2025-05-14 19:22:10 -0400

    updated compareQOIDists to take in as many parameter sets as you want so you can compare QOI distributions freely among different numbers of parameters varying. sobolmain just has base_samples changed because I was rerunning new simulations

--- compareQOIDists.m
+++ compareQOIDists.m
@@ -4,112 +4,99 @@
 % Kyle Adams added comments and adjusted this code to compare QOI
 % distributions resulting from fixing some parameters.
 
-%%changeable here is how you create your bounds. default is
-%%lower bounds are 50% of the nominal value, and upper bounds are 150%
-%% change me %%
+rng(1);  %Setting seed 
+
+%setting up bounds and distributions for sobol sampling
 lower_percentage = 0.5;
 upper_percentage = 1.5;
 base_samples = 175000;
 param_dist = {'Uniform'};
-%varying_params = {'aHI', 'dC', 'dI', 'aIH', 'gH', 'dH', 'bHI', 'bIH', 'bIC', 'KH', 'aHC', 'aCI', 'aIC', 'bHC', 'bCI', 'dA', 'lL', 'sR', 'bIR', 'aRA', 'aIRA', 'lR', 'aIR', 'bRA', 'bIRA', 'bAH', 'dR', 'aAH', 'lH'};
-%numVarying = 29;
-varying_params = {'aCL', 'dL', 'bCL', 'KC', 'gC', 'aIC'};
-numVarying = 6;
-%% ---------- %%
-
-% 1. Get subset varying_params of parameters of the model from parameters.m
 p = parameters();
-p_subset = struct();
-for i = 1:length(varying_params)
-    field = varying_params{i};
-    if isfield(p,field)
-        p_subset.(field) = p.(field);
+
+%defining parameter sets to compare QOI distributions
+param_sets = {
+    'All', fieldnames(p)', ...
+    'Top 6 Influential Parameters Varied', {'aCL', 'dL', 'bCL', 'KC', 'gC', 'aIC'}, ...
+    'Least 29 Influential Parameters Varied', {'aHI', 'dC', 'dI', 'aIH', 'gH', 'dH', 'bHI', 'bIH', 'bIC', ...
+                  'KH', 'aHC', 'aCI', 'bHC', 'bCI', 'aIC', 'dA', 'lL', 'sR', ...
+                  'bIR', 'aRA', 'aIRA', 'lR', 'aIR', 'bRA', 'bIRA', 'bAH', ...
+                  'dR', 'aAH', 'lH'} ...
+    };
+
+num_param_sets = length(param_sets) / 2;
+QOIs = cell(1, num_param_sets);
+labels = cell(1, num_param_sets);
+
+%performing a sensitivity analysis for each parameter set
+for i = 1:num_param_sets
+    set_name = param_sets{2*i - 1};
+    varying_params = param_sets{2*i};
+    labels{i} = set_name;
+
+    %using the name of the parameters from param_sets, extract all
+    %parameter info from parameters.m
+    p_subset = struct();
+    for j = 1:length(varying_params)
+        field = varying_params{j};
+        if isfield(p, field)
+            p_subset.(field) = p.(field);
+        end
     end
+
+    param_names = fieldnames(p_subset);
+    num_params = length(param_names);
+    lowBounds = zeros(1, num_params);
+    upBounds = zeros(1, num_params);
+
+    for j = 1:num_params
+        param = param_names{j};
+        lowBounds(j) = p_subset.(param) * lower_percentage;
+        upBounds(j) = p_subset.(param) * upper_percentage;
+    end
+
+    parsObj.name = param_names';
+    parsObj.lb = num2cell(repmat(-inf, 1, length(parsObj.name)));
+    parsObj.ub = num2cell(inf(1, length(parsObj.name)));
+    parsObj.dist = repmat(param_dist, 1, num_params);
+    parsObj.N = base_samples;
+    parsObj.parameters = arrayfun(@(i) {'lower', lowBounds(i), 'upper', upBounds(i)}, ...
+                                  1:num_params, 'UniformOutput', false);
+
+    %get sobol samples
+    samples = getSamplesSobol(parsObj, false);
+
+    %simulate the QOI
+    QOI = zeros(1, length(samples));
+    pN = cell(1,length(samples));
+    parsName = parsObj.name;
+    parfor ii = 1:length(samples)
+        pN{ii} = updatePars(p, parsName, samples(ii, :));
+        QOI(ii) = qoi(pN{ii});
+    end
+    QOIs{i} = QOI;
 end
-disp(p_subset)
-paramNames = fieldnames(p_subset);
-numParam = length(paramNames);
+smallest_num_model_evals = min(cellfun(@length, QOIs)); %used to take subsets of bigger QOI distributions to compare fairly
 
-lowBounds = zeros(1, numParam); %initializes vector of length numParam with 0s
-upBounds = zeros(1, numParam);
-
-
-%2a. Set up bounds
-for i = 1:numParam
-    param = paramNames{i};
-    lowBounds(i) = p_subset.(param)*lower_percentage;
-    upBounds(i) = p_subset.(param)*upper_percentage;
-end
-
-%storing this info in structure called parsObj, used in getSamplesSobol
-parsObj.name = paramNames'; %transpose so the dimensions of each field match
-parsObj.lb = num2cell(repmat(-inf, 1, length(parsObj.name)));
-parsObj.ub = num2cell(inf(1, length(parsObj.name)));
-parsObj.dist = repmat(param_dist, 1, numParam);
-parsObj.N = base_samples; %how many sobol samples you want
-%below is a field that stores each parameter's bounds
-parsObj.parameters = arrayfun(@(i) {'lower', lowBounds(i), 'upper', upBounds(i)}, 1:numParam, 'UniformOutput', false);
-
-samples = getSamplesSobol(parsObj, false);
-
-%%
-% 3. Simulating model for the desired parameter set and extracting 
-% QOI cells at the end of 30 days
-
-parsName = parsObj.name;
-QOI = zeros(1, length(samples));
-pN = cell(1,length(samples));
-parfor ii = 1:length(samples)
-    pN{ii} = updatePars(p, parsName, samples(ii, :));
-    QOI(ii) = qoi(pN{ii});
-end
-
-%saving QOI with some fixed parameters for histogram comparison
-%QOI_with_fixed_params = QOI;
-QOI_6 = QOI;
-
-%plot histograms of comparing QOI distributions and perform KS test
-plotHistogram(numVarying, QOI_all_varying, QOI_with_fixed_params);
-[h,p, ks2stat] = kstest2(QOI_all_varying, QOI_with_fixed_params);
-
-
-%cdf plots
+%plotting histogram comparisons of QOI distributions
 figure;
 hold on;
-c1 = cdfplot(QOI_with_fixed_params);
-c2 = cdfplot(subset(QOI_all_varying, length(QOI_with_fixed_params)));
-c1.LineWidth = 2;
-c2.LineWidth = 2;
-legend({'All Parameters Varied', sprintf('%d Parameters Varied', numVarying)}, ...
-    'FontSize', 12, 'FontName', 'serif', 'Location', 'northwest');
-hold off;
-
-function p = updatePars(p, parsName, parsValue)
-
-for ii = 1:length(parsName)
-    p.(parsName{ii}) = parsValue(ii);
+colors = {'red', 'black', 'blue'};
+for i = 1:num_param_sets
+    histogram(subset(QOIs{i}, smallest_num_model_evals), ...
+        'EdgeColor', colors{i}, ...
+        'DisplayStyle', 'stairs', ...
+        'LineWidth', 2);
 end
-
-end
-
-function plotHistogram(numVarying, QOI_all, QOI_some)
-figure;
-hold on;
-subset_QOI_all = subset(QOI_all, 1400000);
-histogram(subset_QOI_all,'EdgeColor', 'red', 'DisplayStyle', 'stairs', 'LineWidth', 2);
-histogram(QOI_some,'EdgeColor', 'blue', 'DisplayStyle', 'stairs', 'LineWidth', 2);
-legend({'All Parameters Varied', sprintf('%d Parameters Varied', numVarying)}, ...
-    'FontSize', 12, 'FontName', 'serif', 'Location', 'northwest');
+legend(labels, 'FontSize', 12, 'FontName', 'serif', 'Location', 'northwest');
 xlabel('QOI Value', 'FontSize', 12, 'FontName', 'serif');
 ylabel('Frequency', 'FontSize', 12, 'FontName', 'serif');
-title('Histogram Comparison', 'FontSize', 12, 'FontName', 'serif');
+title('Histogram Comparison of QOI Distributions', 'FontSize', 14, 'FontName', 'serif');
 xlim([0 2e11]);
 hold off;
 
+%update parameters function
+function p = updatePars(p, parsName, parsValue)
+    for ii = 1:length(parsName)
+        p.(parsName{ii}) = parsValue(ii);
+    end
 end
-%saving data
-
-%writematrix(QOI, 'QOIvalues.txt') %saves QOI values
-%writetable(mytable, "SAValues.txt") %saves sensitivity indices
-
-
