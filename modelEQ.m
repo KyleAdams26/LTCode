d2289a918915d265b9c1b6919f37a9ab3116edc6
Helen Moore <105080912+moore-math@users.noreply.github.com>
2025-05-16 19:35:44 -0400

    Update modelEQ.m

--- modelEQ.m
+++ modelEQ.m
@@ -1,35 +1,37 @@
 function modelEQ
-    %initializing colors
-    L_color = [166/255, 107/255, 97/255];
-    A_color = [250/255, 129/255, 113/255]; 
-    Th_color = [176/255, 101/255, 243/255];
-    Tc_color = [252/255, 132/255, 217/255];
-    Tr_color = [139/255, 235/255, 229/255];
-    I_color = [251/255, 186/255, 27/255];
+    % define the colors of the state variables
+    L_color  = [166/255, 107/255,  97/255]; % brown
+    A_color  = [250/255, 129/255, 113/255]; % salmon
+    Th_color = [176/255, 101/255, 243/255]; % purple
+    Tc_color = [252/255, 132/255, 217/255]; % pink
+    Tr_color = [139/255, 235/255, 229/255]; % turquoise
+    I_color  = [251/255, 186/255,  27/255]; % goldenrod
 
-    %load in parameters
+    %load the parameter names and values
     p = parameters();
 
-    %choosing timespan and initial conditions
-    t0 = 0; tfinal = 400; % simulation time in days
+    % set simulation timespan and load the initial conditions
+    t0 = 0; tfinal = 400; % initial and final simulation times in days
     IC = getInitialConditions(); % get the initial values of the model
     IC = struct2cell(IC); IC = [IC{:}];
 
    
-    % The integration settings defined by options
-    options = odeset('RelTol',1e-12,'AbsTol',1e-12) ;
-    %simulates the model 
+    % choose integration settings using "options"
+    options = odeset('RelTol',1e-12,'AbsTol',1e-12);
+    
+    % simulate the model 
     [Tf,Xf] = ode45(@(t, y)odefun(t, y, p),...
         [t0 tfinal], IC, options);
-    %the next lines store the solutions for each variable of the system for plotting
-    LF  = Xf(:,1);
-    AF = Xf(:,2);
+        
+    % store the solutions for each variable for plotting
+    LF   = Xf(:,1);
+    AF   = Xf(:,2);
     ThF  = Xf(:,3);
-    TcF = Xf(:,4);
-    TrF = Xf(:,5);
-    IF = Xf(:,6);
+    TcF  = Xf(:,4);
+    TrF  = Xf(:,5);
+    IF   = Xf(:,6);
    
-    %% Plots Together %%
+    %% Create plots of the variables in a 2x3 grid %%
     % each plot has the same aesthetic and similar labelings
     
     tiledlayout(2,3)
@@ -38,7 +40,7 @@ function modelEQ
     nexttile
     plot(Tf, LF, 'Color', L_color, 'LineWidth',3)
     xlim([0 tfinal])
-    xlabel('Time (Days)')
+    xlabel('Time (days)')
     ylabel('L (cells/\mu L)')
     title('Liver hepatocytes (L) over time')
     ax = gca;
@@ -51,7 +53,7 @@ function modelEQ
     plot(Tf,AF,'Color', A_color,'LineWidth', 3)
     title('Cell Populations')
     xlim([0 tfinal]) 
-    xlabel('Time (Days)')
+    xlabel('Time (days)')
     ylabel('A (cells/\mu L)')
     title('APCs (A) over time')
     ax.XAxis.FontSize = 10;
@@ -61,7 +63,7 @@ function modelEQ
     nexttile
     plot(Tf,TcF, 'Color', Tc_color, 'LineWidth', 3)
     xlim([0 tfinal])
-    xlabel('Time (Days)')
+    xlabel('Time (days)')
     ylabel('T_{C} (cells/\mu L)')
     title('Cytotoxic T cells (T_{C}) over time')
     ax.XAxis.FontSize = 10;
@@ -71,7 +73,7 @@ function modelEQ
     nexttile
     plot(Tf,ThF, 'Color', Th_color,'LineWidth',3 )
     xlim([0 tfinal])
-    xlabel('Time (Days)')
+    xlabel('Time (days)')
     ylabel('T_{H} (cells/\mu L)')
     title('Helper T cells (T_{H}) over time')
     ax.XAxis.FontSize = 10;
@@ -81,7 +83,7 @@ function modelEQ
     nexttile
     plot(Tf,TrF,'Color', Tr_color, 'LineWidth',3)
     xlim([0 tfinal])
-    xlabel('Time (Days)')
+    xlabel('Time (days)')
     ylabel('T_{R} (cells/\mu L)')
     title('Regulatory T cells (T_{R}) over time')
     ax.XAxis.FontSize = 10;
@@ -91,7 +93,7 @@ function modelEQ
     nexttile
     plot(Tf,IF,'Color', I_color,'LineWidth',3)
     xlim([0 tfinal])
-    xlabel('Time (Days)')
+    xlabel('Time (days)')
     ylabel('I (ng/\mu L)')
     title('IL-2 (I) over time')
     ax.XAxis.FontSize = 10;
